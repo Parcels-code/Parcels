88d585a98c48aaacdbab083117144899c76d129e
Danilo Silva <nilodna@gmail.com>
2025-10-16 04:01:53 -0300

    Refactor error message handling in _assert_str_and_python_varname for clarity and consistency

--- src/parcels/_core/utils/string.py
+++ src/parcels/_core/utils/string.py
@@ -2,16 +2,16 @@ from keyword import iskeyword, kwlist
 
 
 def _assert_str_and_python_varname(name):
+
     if not isinstance(name, str):
         raise TypeError(f"Expected a string for variable name, got {type(name).__name__} instead.")
 
+    msg = f"Received invalid Python variable name {name!r}: "
+
     if not name.isidentifier():
-        raise ValueError(
-            f"Received invalid Python variable name {name!r}: not a valid identifier. "
-            f"HINT: avoid using spaces, special characters, and starting with a number."
-        )
+        msg += "not a valid identifier. HINT: avoid using spaces, special characters, and starting with a number."
+        raise ValueError(msg)
+    
     if iskeyword(name):
-        raise ValueError(
-            f"Received invalid Python variable name {name!r}: it is a reserved keyword. "
-            f"HINT: avoid using the following names: {', '.join(kwlist)}"
-        )
+        msg += f"it is a reserved keyword. HINT: avoid using the following names: {', '.join(kwlist)}"
+        raise ValueError(msg)
