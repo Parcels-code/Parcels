e15c46ccbb09dcefda51a85f52f4bfcc818caa00
Erik van Sebille <erikvansebille@gmail.com>
2025-11-21 12:11:44 +0100

    Revert "Add next_dt in RK45"
    This reverts commit 6a67411ab884f36dc934777f6f94dca87554615e.

--- tests/test_advection.py
+++ tests/test_advection.py
@@ -285,13 +285,8 @@ def test_moving_eddy(kernel, rtol):
 
     if kernel == AdvectionRK45:
         fieldset.add_constant("RK45_tol", rtol)
-        MyParticle = Particle.add_variable(Variable("next_dt", dtype="timedelta64[s]", initial=dt))
-    else:
-        MyParticle = Particle
 
-    pset = ParticleSet(
-        fieldset, pclass=MyParticle, lon=start_lon, lat=start_lat, z=start_z, time=np.timedelta64(0, "s")
-    )
+    pset = ParticleSet(fieldset, lon=start_lon, lat=start_lat, z=start_z, time=np.timedelta64(0, "s"))
     pset.execute(kernel, dt=dt, endtime=np.timedelta64(1, "h"))
 
     def truth_moving(x_0, y_0, t):
@@ -330,11 +325,8 @@ def test_decaying_moving_eddy(kernel, rtol):
     if kernel == AdvectionRK45:
         fieldset.add_constant("RK45_tol", rtol)
         fieldset.add_constant("RK45_min_dt", 10 * 60)
-        MyParticle = Particle.add_variable(Variable("next_dt", dtype="timedelta64[s]", initial=dt))
-    else:
-        MyParticle = Particle
 
-    pset = ParticleSet(fieldset, pclass=MyParticle, lon=start_lon, lat=start_lat, time=np.timedelta64(0, "s"))
+    pset = ParticleSet(fieldset, lon=start_lon, lat=start_lat, time=np.timedelta64(0, "s"))
     pset.execute(kernel, dt=dt, endtime=np.timedelta64(23, "h"))
 
     def truth_moving(x_0, y_0, t):
@@ -381,14 +373,12 @@ def test_stommelgyre_fieldset(kernel, rtol, grid_type):
     start_lon = np.linspace(10e3, 100e3, npart)
     start_lat = np.ones_like(start_lon) * 5000e3
 
+    if kernel == AdvectionRK45:
+        fieldset.add_constant("RK45_tol", rtol)
+
     SampleParticle = Particle.add_variable(
         [Variable("p", initial=0.0, dtype=np.float32), Variable("p_start", initial=0.0, dtype=np.float32)]
     )
-
-    if kernel == AdvectionRK45:
-        fieldset.add_constant("RK45_tol", rtol)
-        fieldset.add_constant("RK45_min_dt", 5 * 60)
-        SampleParticle = SampleParticle.add_variable(Variable("next_dt", dtype="timedelta64[s]", initial=dt))
 
     def UpdateP(particles, fieldset):  # pragma: no cover
         particles.p = fieldset.P[particles.time, particles.z, particles.lat, particles.lon]
@@ -404,7 +394,7 @@ def test_stommelgyre_fieldset(kernel, rtol, grid_type):
     [
         (AdvectionRK2, 2e-2),
         (AdvectionRK4, 5e-3),
-        (AdvectionRK45, 1e-3),
+        (AdvectionRK45, 1e-4),
     ],
 )
 @pytest.mark.parametrize("grid_type", ["A"])  # TODO also implement C-grid once available
@@ -423,14 +413,12 @@ def test_peninsula_fieldset(kernel, rtol, grid_type):
     start_lat = np.linspace(3e3, 47e3, npart)
     start_lon = 3e3 * np.ones_like(start_lat)
 
+    if kernel == AdvectionRK45:
+        fieldset.add_constant("RK45_tol", rtol)
+
     SampleParticle = Particle.add_variable(
         [Variable("p", initial=0.0, dtype=np.float32), Variable("p_start", initial=0.0, dtype=np.float32)]
     )
-
-    if kernel == AdvectionRK45:
-        fieldset.add_constant("RK45_tol", rtol)
-        fieldset.add_constant("RK45_min_dt", 5 * 60)
-        SampleParticle = SampleParticle.add_variable(Variable("next_dt", dtype="timedelta64[s]", initial=dt))
 
     def UpdateP(particles, fieldset):  # pragma: no cover
         particles.p = fieldset.P[particles.time, particles.z, particles.lat, particles.lon]
