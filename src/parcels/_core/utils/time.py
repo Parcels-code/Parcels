b1b470e7b96ac35e7e34ded62123de741cde9163
Erik van Sebille <erikvansebille@gmail.com>
2025-12-05 13:13:59 +0100

    Revert "explicit renaming to `is_all_time_in_flt_interval`"
    This reverts commit 2b166b8798cd5d2bb1b9238e299042874a6f1e18.

--- src/parcels/_core/utils/time.py
+++ src/parcels/_core/utils/time.py
@@ -56,7 +56,7 @@ class TimeInterval:
     def __contains__(self, item: T) -> bool:
         return self.left <= item <= self.right
 
-    def is_all_time_in_flt_interval(self, time: float):
+    def is_all_time_in_interval(self, time):
         item = np.atleast_1d(time)
         return (0 <= item).all() and (item <= self.time_length_as_flt).all()
 
