e9947edbcccb384b93a0279f34bff79e8c0ccda1
Joe Schoonover <joe@fluidnumerics.com>
2026-04-06 13:17:31 -0400

    Add test that reproduces the issue described in issue #2568

---
+++ tests/test_uxadvection.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pytest
+import xarray as xr
+
+import parcels
+from parcels import (
+    Field,
+    FieldSet,
+    Particle,
+    ParticleFile,
+    ParticleSet,
+    StatusCode,
+    Variable,
+    VectorField,
+    UxGrid,
+    convert,
+)
+from parcels._core.utils.time import timedelta_to_float
+from parcels._datasets.unstructured.generic import datasets as datasets_unstructured
+
+from parcels.kernels import (
+    AdvectionEE,
+    AdvectionRK2,
+    AdvectionRK4,
+)
+from tests.utils import DEFAULT_PARTICLES
+
+
+@pytest.mark.parametrize("integrator", [AdvectionEE, AdvectionRK2,AdvectionRK4])
+def test_ux_constant_flow_face_centered_2D(integrator):
+    ds = datasets_unstructured["ux_constant_flow_face_centered_2D"]
+    T = np.timedelta64(3600, "s")
+    dt = np.timedelta64(300, "s")
+    dt_s = 300.0
+
+    fieldset = parcels.FieldSet.from_ugrid_conventions(ds, mesh="flat")
+    pset = parcels.ParticleSet(fieldset, lon=[5.0], lat=[5.0])
+    pfile = parcels.ParticleFile(store="test.zarr", outputdt=dt)
+    pset.execute(integrator, runtime=T, dt=dt, output_file=pfile, verbose_progress=False)
+    expected_lon = 8.6
+    np.testing.assert_allclose(pset.lon, expected_lon, atol=1e-5)
+
