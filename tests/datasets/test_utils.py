3f1979fbf25568ad5b6ebba16055e8ac3585de22
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2026-04-07 13:53:39 +0200

    Refactor to json.JSON*coder classes

--- tests/datasets/test_utils.py
+++ tests/datasets/test_utils.py
@@ -1,8 +1,19 @@
+import json
+from datetime import datetime
+
 import pytest
 import xarray as xr
 
 from parcels._datasets import utils
 from parcels._datasets.structured.generic import datasets
+
+
+def test_datetime_encoder_decoder_roundtrip():
+    dt = datetime(2000, 1, 15, 12, 30, 45)
+    data = {"timestamps": [dt, dt], "nested": {"time": dt}, "value": 42}
+    encoded = json.dumps(data, cls=utils._DatetimeEncoder)
+    decoded = json.loads(encoded, cls=utils._DatetimeDecoder)
+    assert decoded == data
 
 
 def test_from_xarray_dataset_dict():
