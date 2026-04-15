0e081238673b50223434d5fe720d4ca8acc83c7a
Joe Schoonover <joe@fluidnumerics.com>
2026-04-15 14:20:15 -0400

    Test the particle file output in addition to the pset.lon

--- tests/test_uxadvection.py
+++ tests/test_uxadvection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import xarray as xr
 
 import parcels
 from parcels._datasets.unstructured.generic import datasets as datasets_unstructured
@@ -11,7 +12,7 @@ from parcels.kernels import (
 
 
 @pytest.mark.parametrize("integrator", [AdvectionEE, AdvectionRK2, AdvectionRK4])
-def test_ux_constant_flow_face_centered_2D(integrator):
+def test_ux_constant_flow_face_centered_2D(integrator, tmp_zarrfile):
     ds = datasets_unstructured["ux_constant_flow_face_centered_2D"]
     T = np.timedelta64(3600, "s")
     dt = np.timedelta64(300, "s")
@@ -19,7 +20,10 @@ def test_ux_constant_flow_face_centered_2D(integrator):
 
     fieldset = parcels.FieldSet.from_ugrid_conventions(ds, mesh="flat")
     pset = parcels.ParticleSet(fieldset, lon=[5.0], lat=[5.0])
-    pfile = parcels.ParticleFile(store="test.zarr", outputdt=dt)
+    pfile = parcels.ParticleFile(store=tmp_zarrfile, outputdt=dt)
     pset.execute(integrator, runtime=T, dt=dt, output_file=pfile, verbose_progress=False)
     expected_lon = 8.6
     np.testing.assert_allclose(pset.lon, expected_lon, atol=1e-5)
+
+    ds_out = xr.open_zarr(tmp_zarrfile)
+    np.testing.assert_allclose(ds_out["lon"][:, -1], expected_lon, atol=1e-5)
