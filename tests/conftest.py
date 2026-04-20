19fbd8dddac5272915a4d9a1d343ecb47a779b41
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2026-04-20 11:51:15 +0200

    Remove old fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,4 @@
 import pytest
-from zarr.storage import MemoryStore
-
-
-@pytest.fixture()
-def tmp_parquet(tmp_path, request):
-    test_name = request.node.name
-    yield tmp_path / f"{test_name}-output.zarr"
-
-
-@pytest.fixture
-def tmp_store():
-    return MemoryStore()
 
 
 @pytest.fixture
