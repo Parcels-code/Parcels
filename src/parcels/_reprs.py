b57e78f2b4fb930a851fb54aaedd1203a71f3c33
Erik van Sebille <erikvansebille@gmail.com>
2026-04-30 10:45:30 +0200

    Fixing repr of particleset

--- src/parcels/_reprs.py
+++ src/parcels/_reprs.py
@@ -97,7 +97,7 @@ def particleset_repr(pset: ParticleSet) -> str:
 def particlesetview_repr(pview: Any) -> str:
     """Return a pretty repr for ParticleSetView"""
     time_string = "not_yet_set" if pview.time is None or np.isnan(pview.time) else f"{pview.time:f}"
-    out = f"P[{pview.trajectory}]: time={time_string}, z={pview.z:f}, lat={pview.lat:f}, lon={pview.lon:f}"
+    out = f"P[{pview.particle_id}]: time={time_string}, z={pview.z:f}, lat={pview.lat:f}, lon={pview.lon:f}"
     vars = [v.name for v in pview._ptype.variables if v.to_write is True and v.name not in ["lon", "lat", "z", "time"]]
     for var in vars:
         out += f", {var}={getattr(pview, var):f}"
