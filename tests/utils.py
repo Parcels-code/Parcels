0241c28c06a7d1fd3b63b5b844ca46e341bf773f
Erik van Sebille <erikvansebille@gmail.com>
2026-05-04 11:11:36 +0200

    Adding comment on cftime

--- tests/utils.py
+++ tests/utils.py
@@ -161,6 +161,7 @@ def assert_cftime_like_particlefile(parquet_path: Path) -> None:
     df = parcels.read_particlefile(parquet_path, decode_times=True)
 
     # check first value (and hence rest of array) is what we expect
+    # TODO explore use of cftime in polars
     assert isinstance(df["time"][0], (cftime.datetime, datetime)), (
         "CF-time values in Parquet did not get properly decoded. Are the attributes correct?"
     )
