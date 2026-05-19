e441cc0beee404a780615c3ba98e92e5e5b70057
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2026-05-19 14:51:20 +0200

    Add isel method to Xarray accessor

--- src/parcels/_sgrid/accessor.py
+++ src/parcels/_sgrid/accessor.py
@@ -1,8 +1,10 @@
 import itertools
-from collections.abc import Mapping
-from typing import Any
+from collections.abc import Mapping, Sequence
+from typing import Any, Literal
 
 import xarray as xr
+
+from parcels._python import invert_non_unique_mapping
 
 from .core import SGrid2DMetadata, SGrid3DMetadata, get_n_faces, parse_grid_attrs
 
@@ -10,12 +12,15 @@ from .core import SGrid2DMetadata, SGrid3DMetadata, get_n_faces, parse_grid_attr
 @xr.register_dataset_accessor("sgrid")
 class SgridAccessor:
     def __init__(self, xarray_obj):
-        self._ds = xarray_obj
+        self._ds: xr.Dataset = xarray_obj
 
     @property
-    def metadata(self) -> SGrid2DMetadata | SGrid3DMetadata:
+    def metadata(self) -> SGrid2DMetadata:
         grid_da = self._get_grid_topology()
-        return parse_grid_attrs(grid_da.attrs)
+        grid = parse_grid_attrs(grid_da.attrs)
+        if isinstance(grid, SGrid3DMetadata):
+            raise NotImplementedError("Support for 3D SGRID metadata not supported.")
+        return grid
 
     def rename(self, name_dict: dict[str, str]) -> xr.Dataset:
         """Similar to Xarray's rename functionality - but also updates the SGRID metadata attributes."""
@@ -38,19 +43,31 @@ class SgridAccessor:
             )
         return grid_da
 
-    def isel(self, indexers: Mapping[Any, Any] | None = None, **indexers_kwargs):
-        if indexers is None:
-            indexers = {}
+    def isel(self, indexers: Mapping[str, Any] | None = None, **indexers_kwargs):
+        """TODO: Docstring"""
+        if indexers_kwargs != {}:
+            if indexers is not None:
+                raise ValueError("Cannot provide both positional and keyword argument to .isel .")
+            indexers = indexers_kwargs
 
-        for k, indexer in itertools.chain(indexers.items(), indexers_kwargs.items()):
+        assert indexers is not None
+
+        for k, indexer in indexers.items():
             if not isinstance(indexer, slice):
                 raise NotImplementedError(
                     f"sgrid.isel() only works on `slice` objects for the timebeing. Got indexer {indexer!r} for {k!r}"
                 )
 
-        _meta = self.metadata
+        metadata = self.metadata
 
-        ...
+        _assert_not_indexing_along_same_axis(indexers, metadata)
+        _assert_all_isel_along_axis(indexers.keys(), metadata)
+
+        indexers = _complete_isel_indexing(indexers, metadata, self._ds.dims.keys())
+
+        ds = self._ds.isel(indexers=indexers)
+        assert_metadata_ds_consistency(ds, metadata)
+        return ds
 
 
 def assert_metadata_ds_consistency(ds: xr.Dataset, metadata: SGrid2DMetadata):
@@ -85,4 +102,59 @@ def assert_metadata_ds_consistency(ds: xr.Dataset, metadata: SGrid2DMetadata):
 class SGridDatasetInconsistency(Exception):
     """Attached metadata is not compatible with Xarray dataset"""
 
-    ...
+    pass
+
+
+def _get_dim_to_axis_mapping(grid: SGrid2DMetadata) -> dict[str, Literal["X", "Y", "Z"]]:
+    fnp_x = grid.face_dimensions[0]
+    fnp_y = grid.face_dimensions[1]
+    fnp_z = grid.vertical_dimensions[0] if grid.vertical_dimensions is not None else None
+
+    d = {
+        fnp_x.node: "X",
+        fnp_x.face: "X",
+        fnp_y.node: "Y",
+        fnp_y.face: "Y",
+    }
+    if fnp_z is not None:
+        d.update({fnp_z.node: "Z", fnp_z.face: "Z"})
+    return d
+
+
+def _assert_not_indexing_along_same_axis(indexers: Mapping[Any, Any], metadata: SGrid2DMetadata) -> None:
+    dim_to_axis = _get_dim_to_axis_mapping(metadata)
+    indexer_dim_to_axis = {dim: dim_to_axis.get(dim) for dim in indexers}
+
+    indexer_axis_to_dim = invert_non_unique_mapping(indexer_dim_to_axis)
+    for axis, dims in indexer_axis_to_dim.items():
+        if axis is None:
+            continue
+
+        if len(dims) > 1:
+            msg = f"Dims {dims} are on the same axis {axis!r} according to SGRID metadata - cannot simultaneously index along multiple dimensions in the same axis."
+            raise ValueError(msg)
+
+
+def _assert_all_isel_along_axis(index_dims: Sequence[str], metadata: SGrid2DMetadata):
+    dim_to_axis = _get_dim_to_axis_mapping(metadata)
+    for dim in index_dims:
+        try:
+            dim_to_axis[dim]
+        except KeyError as e:
+            raise ValueError(
+                f"Cannot use SGRID accessor to .isel non-spatial (/SGRID related) dimension {dim!r}."
+            ) from e
+
+
+def _complete_isel_indexing(
+    indexers: Mapping[Any, Any], grid: SGrid2DMetadata, dims_in_dataset: Sequence[str]
+) -> Mapping[Any, Any]:
+    """Copies indexers to the other dataset dimensions defined on the same axis."""
+    ret = {}
+    dim_to_axis = _get_dim_to_axis_mapping(grid)
+    indexers_by_axis = {dim_to_axis[dim]: indexer for dim, indexer in indexers.items()}
+
+    for dim, axis in dim_to_axis.items():
+        if dim in dims_in_dataset and axis in indexers_by_axis:
+            ret[dim] = indexers_by_axis[axis]
+    return ret
--- tests/sgrid/test_accessor.py
+++ tests/sgrid/test_accessor.py
@@ -4,6 +4,7 @@ import xarray as xr
 from hypothesis import assume, given
 
 import parcels._strategies as pst
+from parcels._datasets.structured.generic import datasets_sgrid
 from parcels._datasets.structured.strategies import sgrid_dataset
 from parcels._sgrid import SGrid2DMetadata
 from parcels._sgrid.accessor import SGridDatasetInconsistency, assert_metadata_ds_consistency
@@ -36,16 +37,16 @@ def test_assert_metadata_ds_consistency(ds):
     assert_metadata_ds_consistency(ds, metadata)
 
 
-@given(ds=sgrid_dataset(), dim=st.sampled_from(['face_dimension1', "face_dimension2", "vertical_dimension"]))
+@given(ds=sgrid_dataset(), dim=st.sampled_from(["face_dimension1", "face_dimension2", "vertical_dimension"]))
 def test_assert_metadata_ds_consistency_dropped_dim(ds, dim):
     # dropping one of the SGRID dimensions still results in a consistent dataset
     metadata: SGrid2DMetadata = ds.sgrid.metadata
 
     if dim == "face_dimension1":
         fnp = metadata.face_dimensions[0]
-    elif dim=="face_dimension2":
+    elif dim == "face_dimension2":
         fnp = metadata.face_dimensions[1]
-    elif dim=="vertical_dimension":
+    elif dim == "vertical_dimension":
         assume(metadata.vertical_dimensions is not None)
         assert metadata.vertical_dimensions is not None
         fnp = metadata.vertical_dimensions[0]
@@ -58,15 +59,15 @@ def test_assert_metadata_ds_consistency_dropped_dim(ds, dim):
     assert_metadata_ds_consistency(ds, metadata)
 
 
-@given(ds=sgrid_dataset(), dim=st.sampled_from(['face_dimension1', "face_dimension2", "vertical_dimension"]))
+@given(ds=sgrid_dataset(), dim=st.sampled_from(["face_dimension1", "face_dimension2", "vertical_dimension"]))
 def test_assert_metadata_ds_consistency_failures(ds, dim):
     metadata: SGrid2DMetadata = ds.sgrid.metadata
 
     if dim == "face_dimension1":
         fnp = metadata.face_dimensions[0]
-    elif dim=="face_dimension2":
+    elif dim == "face_dimension2":
         fnp = metadata.face_dimensions[1]
-    elif dim=="vertical_dimension":
+    elif dim == "vertical_dimension":
         assume(metadata.vertical_dimensions is not None)
         assert metadata.vertical_dimensions is not None
         fnp = metadata.vertical_dimensions[0]
@@ -85,13 +86,32 @@ def test_assert_metadata_ds_consistency_failures(ds, dim):
         assert_metadata_ds_consistency(ds, metadata)
 
 
-@given(selection_axis=st.sampled_from(["X", "Y", "Z"]), ds=sgrid_dataset(), slice_=st.slices(4))
-def test_isel(selection_axis, ds, slice_):
-    # TODO: Add skip if Z but no Z dimension in ds
+@pytest.mark.parametrize("ds", [pytest.param(ds, id=id_) for id_, ds in datasets_sgrid.items()])
+@pytest.mark.parametrize("slice_", [slice(None, None, 3), slice(2, -3)])
+@pytest.mark.parametrize(
+    "node_dim, face_dim", [("node_dimension1", "face_dimension1"), ("node_dimension2", "face_dimension2")]
+)
+def test_isel(ds, slice_, node_dim, face_dim):
+    # TODO: Extend to padding BOTH and NONE by updating datasets_sgrid
 
-    # select nodes in axis direction
-    # assert consistent
+    assert ds.dims[node_dim] == ds.dims[face_dim]
 
-    # select edges
-    # assert consistent
-    ...
+    ds_trimmed = ds.sgrid.isel({node_dim: slice_})
+
+    assert ds_trimmed.dims[node_dim] == ds_trimmed.dims[face_dim]
+
+    # Assert that other dims haven't been affected
+    for dim, size_before in ds.dims.items():
+        if dim in (node_dim, face_dim):
+            continue
+        size_after = ds_trimmed.dims[dim]
+        assert size_before == size_after
+
+
+@pytest.mark.parametrize("ds", [datasets_sgrid["ds_2d_padded_high"]])
+def test_isel_invalid(ds):
+    with pytest.raises(ValueError, match="Cannot use SGRID accessor to .isel non-spatial \(/SGRID related\) dimension.*"):
+        ds.sgrid.isel(time=slice(None))
+
+    with pytest.raises(ValueError, match="Dims .* are on the same axis .* according to SGRID metadata.*"):
+        ds.sgrid.isel(node_dimension1=slice(None), face_dimension1=slice(None))
