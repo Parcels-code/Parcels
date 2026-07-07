69fa293e16139b4f1dc62e2a6c8d5d040f247203
Maureen Cohen <maureenjcohen@gmail.com>
2026-07-07 16:11:30 +0100

    Applied SphericalMesh/radius imports to Uxgrid as well.

--- src/parcels/_core/uxgrid.py
+++ src/parcels/_core/uxgrid.py
@@ -7,6 +7,7 @@ import uxarray as ux
 
 from parcels._core.basegrid import BaseGrid
 from parcels._core.index_search import GRID_SEARCH_ERROR, _search_1d_array, uxgrid_point_in_cell
+from parcels._core.mesh import SphericalMesh
 from parcels._typing import assert_valid_mesh
 
 _UXGRID_AXES = Literal["Z", "FACE"]
@@ -41,7 +42,12 @@ class UxGrid(BaseGrid):
         if z.ndim != 1:
             raise ValueError("z must be a 1D array of vertical coordinates")
         self.z = z
-        self._mesh = mesh
+        if isinstance(mesh, SphericalMesh):
+            self._mesh = "spherical"
+            self._radius = mesh.radius
+        else:
+            self._mesh = mesh
+            self._radius = None
         self._spatialhash = None
 
         assert_valid_mesh(mesh)
@@ -72,6 +78,14 @@ class UxGrid(BaseGrid):
             return len(self.z.values)
         elif axis == "FACE":
             return self.uxgrid.n_face
+        
+    @property
+    def deg2m(self) -> float:
+        """ Metres per degree of arc for this grid's mesh. """
+        if self._radius is None:
+            return 1852 * 60.0
+        else:
+            return self._radius * np.pi / 180.0
 
     def search(self, z, y, x, ei=None, tol=1e-6):
         """
