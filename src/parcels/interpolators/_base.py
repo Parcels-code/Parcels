5c9e33725f68e78b8769f79ba686484106559cb1
Nick Hodgskin <36369090+VeckoTheGecko@users.noreply.github.com>
2026-07-08 17:01:57 +0200

    FEAT: Add `fieldset.describe()` (#2729)
    * Add reprs to base classes ScalarInterpolator and VectorInterpolator
    
    * Add tabulate dependency
    
    * Add pandas-stubs
    
    * Add fieldset.describe
    
    * Fix mypy errors
    
    * Self-review
    
    * Add constant_field to example
    
    * Update header
    
    * Rename to "Context"
    
    * Add mesh and tiem interval info
    
    * Make "name" the first column
    
    * Update column name
    
    * Add xfail
    
    * Add _mesh to BaseGrid
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/parcels/interpolators/_base.py
+++ src/parcels/interpolators/_base.py
@@ -7,8 +7,14 @@ class ScalarInterpolator(ABC):
     def interp(self, particle_positions, grid_positions, field) -> Any:  #! API a WIP
         ...
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}(...)"
+
 
 class VectorInterpolator(ABC):
     @abstractmethod
     def interp(self, particle_positions, grid_positions, vectorfield) -> Any:  #! API a WIP
         ...
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(...)"
