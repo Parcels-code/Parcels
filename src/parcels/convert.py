b1bae5ead3dba619be2d4c359374a7ac86743cb9
Erik van Sebille <erikvansebille@gmail.com>
2026-07-28 15:46:33 +0200

    Fixing xr.merge warning

--- src/parcels/convert.py
+++ src/parcels/convert.py
@@ -421,7 +421,7 @@ def mitgcm_to_sgrid(*, fields: dict[str, xr.Dataset | xr.DataArray], coords: xr.
 
     coords = _pick_expected_coords(coords, _MITGCM_EXPECTED_COORDS)
 
-    ds = xr.merge(list(fields.values()) + [coords])
+    ds = xr.merge(list(fields.values()) + [coords], compat="override")
     ds.attrs.clear()  # Clear global attributes from the merging
 
     ds = _maybe_rename_variables(ds, _MITGCM_VARNAMES_MAPPING)
